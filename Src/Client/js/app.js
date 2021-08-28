ffd8c9b20a69330baaf2f08ee9b2fc1962e43127
Struanjh <Struanjh@gmail.com>
2021-08-28 17:13:47 +0100

    Started working on JS Features

---
+++ Src/Client/js/app.js
@@ -0,0 +1,14 @@
+
+//FUNCTIONALITy - Smooth Scroll Behaviour
+
+alert("Script working!");
+//Store Results element in variable//
+const results = document.querySelector('nav .results');
+
+results.addEventListener('click', smoothScroll);
+
+function smoothScroll {
+    alert('It was clicked!');
+}
+
+
