7e2c937a1d9f4a6a4293504ee6700f5fa47f2a48
Struanjh <Struanjh@gmail.com>
2021-08-28 21:19:47 +0100

    Trouble-shooting Smooth Scroll JS Functionality

--- Src/Client/js/app.js
+++ Src/Client/js/app.js
@@ -1,14 +1,31 @@
 
 //FUNCTIONALITy - Smooth Scroll Behaviour
 
-alert("Script working!");
-//Store Results element in variable//
-const results = document.querySelector('nav .results');
+//alert("Script working!");//
 
-results.addEventListener('click', smoothScroll);
+//NEW//
+//STORE navitems in list//
+const navItems = document.querySelectorAll('nav li');
+const divs = document.querySelectorAll('div');
 
-function smoothScroll {
-    alert('It was clicked!');
+//Note below function assumes the target section has same class or id name as the link being clicked on
+//Loop through each navItem
+for (navItem of navItems) {
+    //Set clickTarget as current navItem
+    const clickTarget = navItem;
+    //Set scrollTarget as div with corresponding className
+    //-> 2 Steps to do this
+    //1 Store className of current navItem in variable
+    const targetClass = navItem.className;
+    //2 Select div with equivalent className to current navItem
+    const scrollTarget = divs.targetClass;
+    //Call Smoothscroll function
+    smoothScroll(clickTarget, scrollTarget);
 }
 
-
+function smoothScroll(clickTarget, scrollTarget) {
+    clickTarget.addEventListener('click', (e)=>{
+        e.preventDefault;
+        scrollTarget.scrollIntoView({behaviour: "smooth"})
+    })
+}
