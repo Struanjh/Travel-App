44d428bccde965b109fe05b87757539082be044e
Struanjh <Struanjh@gmail.com>
2021-09-18 11:38:33 +0100

    Removing comments

--- src/client/js/countDown.js
+++ src/client/js/countDown.js
@@ -1,25 +1,5 @@
 
 
-//const currentDate = getcurrDate(sp);
-/* function getcurrDate(sp) {
-        today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth()+1; //JS counts month 0-11
-        let yyyy = today.getFullYear();
-        
-        if(dd<10) dd='0'+dd;
-        if(mm<10) mm='0'+mm;
-        return (mm+sp+dd+sp+yyyy);
-        console.log(curday('/'));
-        console.log(curday('-'));
-} */
-
-//Countdown function calculates number of days between todays date and departure date
-/* function countDown(departDate, currentDate) {
-    const daystoDeparture =  currentDate - departDate;
-} */
-
-
 //Calculate One Day in milliseconds
 const oneDay = 1000*60**24;
 //Gives current time
