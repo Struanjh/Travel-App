5b5182ad12be70b2dabb3e217f61c032af0d2aba
Struanjh <Struanjh@gmail.com>
2021-11-14 12:07:59 +0000

    FEAT: Server.js test with JEST

--- _tests_/server.test.js
+++ _tests_/server.test.js
@@ -1,2 +1,16 @@
 
-//
+
+const request = require('supertest');
+const app = require('../src/server/server');
+
+//Test if the server.js responds with successful status - 200 - when post request is made to weatherBit API endpoint
+describe('Test WeatherBit Post EndPoint', () => {
+    test('get weather data', async () => {
+        const res = await request(app)
+            .post('/callWeatherBit')
+            .send({
+                req: `https://api.weatherbit.io/v2.0/current?lat=${req.body.latitude}&lon=${req.body.longitude}&key=${weatherBitKey}`,
+            });
+        expect(res.statusCode).toEqual(200);
+    });
+});
